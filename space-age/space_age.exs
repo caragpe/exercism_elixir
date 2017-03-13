4fcc1ed87f3f37bb942ff4a91e9a0a143c9f3578
carlosagullo <carlos.agullo@gmail.com>
2017-03-13 01:22:02 +0000

    Improved version of space-age

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -1,6 +1,17 @@
 defmodule SpaceAge do
   @type planet :: :mercury | :venus | :earth | :mars | :jupiter
                 | :saturn | :uranus | :neptune
+                
+  @planet_conversion_factor %{
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
 
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
@@ -8,17 +19,10 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-    earth_time = seconds/31557600
-    case planet do
-      :earth -> earth_time
-      :mercury -> earth_time/0.2408467
-      :venus-> earth_time/0.61519726
-      :mars -> earth_time/1.8808158
-      :jupiter -> earth_time/11.862615
-      :saturn -> earth_time/29.447498
-      :uranus -> earth_time/84.016846
-      :neptune -> earth_time/164.79132
-      _ -> "Planet not valid."
-    end
+    years_on_earth(seconds)/@planet_conversion_factor[planet]
+  end
+  
+  def years_on_earth(seconds) do
+    seconds/31557600
   end
 end
